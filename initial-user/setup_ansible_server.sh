f7b6fbb14f00aae8025d9f5a73fe50d7b05d816f
George Oremo <george.oremo@gmail.com>
2022-10-07 22:46:51 +0300

    -

--- initial-user/setup_ansible_server.sh
+++ initial-user/setup_ansible_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # INSTALL ANSIBLE SERVER REQUIREMENTS
-sudo sh setup_pub_cert.sh
+sudo sh setup_pub_cert.sh -n ansibleServer -d /var/nfs/p_key
 sudo apt upgrade -y
 sudo apt install ansible -y
 ansible-playbook playbook03.yml
