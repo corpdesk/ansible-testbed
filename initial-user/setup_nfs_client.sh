920b72745137f95a54632dd93b7a01612b10f780
George Oremo <george.oremo@gmail.com>
2022-10-07 14:16:14 +0300

    params for setup-nfs_client.sh

--- initial-user/setup_nfs_client.sh
+++ initial-user/setup_nfs_client.sh
@@ -1,7 +1,47 @@
-    # variable input:
-    # host_ip, server shared dir:  /var/nfs/p-key, local mounting point: /nfs/p-key
-    sudo apt update -y
-    sudo apt install nfs-common -y
-    sudo mkdir -p /nfs/p-key
-    sudo mount host_ip:/var/nfs/p-key /nfs/p-key
+#!/bin/bash
+
+# Usage:
+# sh setup_nfs_client.sh -a ansibleServer -d sharedDirectory -p mountingPoint
+# Example:
+# sh setup_nfs_client.sh -a 192.168.1.155 -d /var/nfs/p-key -p /nfs/p-key
+
+helpFunction()
+{
+    echo ""
+    echo "Usage: $0 -a ansibleServer -d sharedDirectory -p mountingPoint"
+    echo "Example: $0 -a 192.168.1.155 -d /var/nfs/p-key -p /nfs/p-key"
+    echo -e "\t-a hostname or ip for ansible server"
+    echo -e "\t-d shared directory"
+    echo -e "\t-p mounting point"
+    exit 1 # Exit script after printing help
+}
+
+while getopts "a:d:p:" opt
+do
+    case "$opt" in
+        a ) ansibleServer="$OPTARG" ;;
+        d ) sharedDirectory="$OPTARG" ;;
+        p ) mountingPoint="$OPTARG" ;;
+        ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+    esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$ansibleServer" ] || [ -z "$sharedDirectory" ] || [ -z "$mountingPoint" ]
+then
+    echo "Some or all of the parameters are empty";
+    helpFunction
+fi
+
+# Begin script in case all parameters are correct
+echo "$ansibleServer"
+echo "$sharedDirectory"
+echo "$mountingPoint"
+
+# variable input:
+# host_ip, server shared dir:  /var/nfs/p-key, local mounting point: /nfs/p-key
+sudo apt update -y
+sudo apt install nfs-common -y
+sudo mkdir -p /nfs/p-key
+sudo mount "$ansibleServer":"$sharedDirectory" "$mountingPoint"
 
