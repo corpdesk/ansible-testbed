390a6090d4c179d7ed64301e7604f34e0165dec9
George Oremo <george.oremo@gmail.com>
2022-10-08 09:52:53 +0300

    -

--- initial-user/setup_initial_user.sh
+++ initial-user/setup_initial_user.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# sudo sh setup_initial_user.sh
 # INSTALL ANSIBLE SERVER REQUIREMENTS
 # sudo useradd -m -s /bin/bash devops
-# sudo adduser devops
-sudo adduser devops sudo
+sudo adduser devops
+usermod -aG sudo devops
 sudo id devops
 su -c devops
 # useradd -m -s /bin/bash -p
