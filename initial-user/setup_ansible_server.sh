8fa112c2dde2bcc9615b40bde451ebdf9131ab73
George Oremo <george.oremo@gmail.com>
2022-10-08 09:50:23 +0300

    setup initial user

--- initial-user/setup_ansible_server.sh
+++ initial-user/setup_ansible_server.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # INSTALL ANSIBLE SERVER REQUIREMENTS
+sudo sh setup_initial_user.sh
+echo "whoami:"
+whomi
 sudo sh setup_pub_cert.sh -n ansibleServer -d /var/nfs/p_key
 sudo apt upgrade -y
 sudo apt install ansible -y
-ansible-playbook playbook03.yml
+echo "whoami:"
+whomi
+ansible 192.168.1.137 -m ping
+# ansible-playbook playbook03.yml
---
+++ initial-user/setup_initial_user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# INSTALL ANSIBLE SERVER REQUIREMENTS
+# sudo useradd -m -s /bin/bash devops
+# sudo adduser devops
+sudo adduser devops sudo
+sudo id devops
+su -c devops
+# useradd -m -s /bin/bash -p
+# useradd -m -p \$6\$QGFip3kXOicYeuKf\$pq3AMKWm9G6/iWtu10G6ciExPjRNcGZRL5Gni6zEHg46juPx4ZSSPkBMZLAF/WBfclfDbuSi4KXGW7b4hg1pH/ -s /bin/bash devops
+
+# ansible-playbook playbook03.yml
