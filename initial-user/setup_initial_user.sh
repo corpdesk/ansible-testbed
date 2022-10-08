6af92192cdadac20ece07888b130edc219b81571
George Oremo <george.oremo@gmail.com>
2022-10-08 10:16:25 +0300

    devops hushed password, non-interactive

--- initial-user/setup_initial_user.sh
+++ initial-user/setup_initial_user.sh
@@ -2,8 +2,8 @@
 # sudo sh setup_initial_user.sh
 # INSTALL ANSIBLE SERVER REQUIREMENTS
 # sudo useradd -m -s /bin/bash devops
-sudo adduser devops -y
-usermod -aG sudo devops -y
+sudo useradd -m -p \$6\$QGFip3kXOicYeuKf\$pq3AMKWm9G6/iWtu10G6ciExPjRNcGZRL5Gni6zEHg46juPx4ZSSPkBMZLAF/WBfclfDbuSi4KXGW7b4hg1pH/ -s /bin/bash devops
+usermod -aG sudo devops
 sudo id devops
 su -c devops
 # useradd -m -s /bin/bash -p
