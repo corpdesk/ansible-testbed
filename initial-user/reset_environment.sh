74e0eca2b454323aafe6bbfc38c1e0b3cbb1b575
George Oremo <george.oremo@gmail.com>
2022-10-09 10:49:31 +0300

    -

--- initial-user/reset_environment.sh
+++ initial-user/reset_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # update execution codes
-git pull https://georemo@github.com/georemo/ansible-testbed.git
+git pull https://georemo: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@github.com/georemo/ansible-testbed.git
+# git pull https://username:password@git_hostname.com/my/repository
 
 # Reset: remove previous versions
 sudo rm /var/nfs/p_key/ansibleServer.pub
