a51dcaa0a8702082a389a54e8c852ce6b0c58cc8
George Oremo <george.oremo@gmail.com>
2022-10-20 19:25:59 +0300

    -

--- initial-user/install_vagrant.sh
+++ initial-user/install_vagrant.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install vagrant -y
+# wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+# echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+# sudo apt update && sudo apt install vagrant -y
+
+sudo apt update
+sudo apt upgrade
+sudo apt install virtualbox
