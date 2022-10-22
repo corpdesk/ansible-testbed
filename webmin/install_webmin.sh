bc16c936ab16a6050adbebc12b37c9c79c6c33e7
George Oremo <george.oremo@gmail.com>
2022-10-22 21:43:33 +0300

    -

--- webmin/install_webmin.sh
+++ webmin/install_webmin.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sudo apt update
+sudo apt update
+sudo apt install curl
 curl -fsSL https://download.webmin.com/jcameron-key.asc | sudo gpg --dearmor -o /usr/share/keyrings/webmin.gpg
 # add line to /etc/apt/sources.list: 
 sudo echo "deb [signed-by=/usr/share/keyrings/webmin.gpg] http://download.webmin.com/download/repository sarge con" >> /etc/apt/sources.list
-sudo apt update
-sudo apt install curl webmin
+sudo apt install webmin
