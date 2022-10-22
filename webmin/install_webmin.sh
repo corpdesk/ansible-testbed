134c3ef18a9e478f71105ed62e6dcd406b24ecee
George Oremo <george.oremo@gmail.com>
2022-10-22 21:53:51 +0300

    avoid multiple config setting for webmin install

--- webmin/install_webmin.sh
+++ webmin/install_webmin.sh
@@ -3,8 +3,10 @@
 
 sudo apt update
 sudo apt install curl
-curl -fsSL https://download.webmin.com/jcameron-key.asc | sudo gpg --dearmor -o /usr/share/keyrings/webmin.gpg -y
+curl -fsSL https://download.webmin.com/jcameron-key.asc | sudo gpg --dearmor -o /usr/share/keyrings/webmin.gpg
+# delete existing similar config
+sed -i '/download.webmin/d' /etc/apt/sources.list
 # add line to /etc/apt/sources.list: 
-sudo echo "deb [signed-by=/usr/share/keyrings/webmin.gpg] http://download.webmin.com/download/repository sarge con" >> /etc/apt/sources.list
+sudo echo "deb [signed-by=/usr/share/keyrings/webmin.gpg] http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
 sudo apt update
 sudo apt install webmin
