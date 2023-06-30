f9fc6cfb1e5bfce41bad97660354f14f4b594ddc
georemo <george.oremo@gmail.com>
2023-06-30 14:49:23 +0300

    updated ansible-cd for cloud-brix

--- shared-files/reset_environment.sh
+++ shared-files/reset_environment.sh
@@ -4,12 +4,11 @@
 # update execution codes
 # git pull https://georemo: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@github.com/georemo/ansible-testbed.git
 # git pull https://username:password@git_hostname.com/my/repository
-sudo apt-get update && sudo upgrade -y
+sudo apt update
+sudo apt upgrade -y
+sudo snap lxd
 sudo sh remove_devops.sh
 
-# allow rsync
-sudo ufw allow from 192.168.1.0/24 to any port 873
-sudo ufw allow from 192.168.1.0/24 to any port 8443
 
 # Reset: remove previous versions
 # sudo rm /var/nfs/share/ansibleServer.pub
@@ -25,7 +24,12 @@ sudo apt-get install git net-tools openssh-server tree fish jq zfsutils-linux tr
 # sudo snap install lxd --channel=latest/stable
 # sudo snap refresh lxd --channel=latest/stable
 sudo service ssh restart
+# allow ssh
 sudo ufw allow from 192.168.0.0/24 to any port 22
+# allow rsync
+sudo ufw allow from 192.168.0.0/24 to any port 873
+# allow lxd connection
+sudo ufw allow from 192.168.0.0/24 to any port 8443
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
 sudo sed -i -E 's/#?PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo sed -i -E 's/#?ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
