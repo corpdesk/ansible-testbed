681f4194980dc46e609eae70f83d49c42befcdd5
georemo <george.oremo@gmail.com>
2023-08-01 20:23:54 +0300

    -

--- shared-files/setup_initial_user.sh
+++ shared-files/setup_initial_user.sh
@@ -3,33 +3,35 @@
 
 # reset environment
 sudo sh /tmp/reset_environment.sh
+initialUser="devops"
+pswd="yU0B14NC1PdE"
+
 
 # INSTALL ANSIBLE SERVER REQUIREMENTS
 
 
-# check if devops dir exits
-if [ -d "/home/devops/" ] 
+# check if $initialUser dir exits
+if [ -d "/home/$initialUser/" ] 
 then
-    echo "user devops esists" 
+    echo "user $initialUser esists" 
     sleep 0
 else
-    # sudo useradd -m -s /bin/bash devops
-    echo "creating devops user (non-inteructive, with preset hushed password):"
-    sudo useradd -m -p \$6\$QGFip3kXOicYeuKf\$pq3AMKWm9G6/iWtu10G6ciExPjRNcGZRL5Gni6zEHg46juPx4ZSSPkBMZLAF/WBfclfDbuSi4KXGW7b4hg1pH/ -s /bin/bash devops
-    sudo mkdir -p /home/devops/.ssh
-    sudo chown devops /home/devops/.ssh
-    sudo chmod 700 /home/devops/.ssh
-    echo "escalate the devops to sudoer:"
-    usermod -aG sudo devops
-    sudo id devops
-    # no password for devops as a sudoer
-    sudo echo "devops ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+    # sudo useradd -m -s /bin/bash $initialUser
+    echo "creating $initialUser user (non-inteructive, with preset hushed password):"
+    # sudo useradd -m -p \$6\$QGFip3kXOicYeuKf\$pq3AMKWm9G6/iWtu10G6ciExPjRNcGZRL5Gni6zEHg46juPx4ZSSPkBMZLAF/WBfclfDbuSi4KXGW7b4hg1pH/ -s /bin/bash $initialUser
+    useradd -m -p $(openssl passwd -1 $pswd) $initialUser
+    echo "escalate the $initialUser to sudoer:"
+    usermod -aG sudo $initialUser
+    sudo id $initialUser
+    # no password for $initialUser as a sudoer
+    sudo echo "$initialUser ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
     # useradd -m -s /bin/bash -p
-    # useradd -m -p \$6\$QGFip3kXOicYeuKf\$pq3AMKWm9G6/iWtu10G6ciExPjRNcGZRL5Gni6zEHg46juPx4ZSSPkBMZLAF/WBfclfDbuSi4KXGW7b4hg1pH/ -s /bin/bash devops
-
-    sudo mkdir /home/devops/vagrant-deploy
-    sudo chmod -R 755 /home/devops/vagrant-deploy
-    chown -R devops /home/devops/vagrant-deploy
+    # useradd -m -p \$6\$QGFip3kXOicYeuKf\$pq3AMKWm9G6/iWtu10G6ciExPjRNcGZRL5Gni6zEHg46juPx4ZSSPkBMZLAF/WBfclfDbuSi4KXGW7b4hg1pH/ -s /bin/bash $initialUser
+    sudo mkdir -p /home/$initialUser/.ssh
+    sudo chown $initialUser /home/$initialUser/.ssh
+    sudo mkdir /home/$initialUser/vagrant-deploy
+    sudo chmod -R 755 /home/$initialUser/vagrant-deploy
+    chown -R $initialUser /home/$initialUser/vagrant-deploy
 fi
 
 
