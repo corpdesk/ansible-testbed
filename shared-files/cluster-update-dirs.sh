a7cfa0f3c5e406821bc3341ce4310b23a16ce96e
georemo <george.oremo@gmail.com>
2023-08-07 02:09:48 +0300

    -

--- shared-files/cluster-update-dirs.sh
+++ shared-files/cluster-update-dirs.sh
@@ -5,29 +5,29 @@ sudo -H -u devops bash -c '
 
 if [ -d "/home/devops/ansible-testbed" ] 
 then
-    echo "--------cloud-brix files will be updated"
+    echo "--------cloud-brix files will be updated at $hostname"
     cd /home/devops/ansible-testbed
     git pull
     cd /home/devops/
 else
-    echo "--------updating source files"
+    echo "--------updating source files at $hostname"
     git clone https://github.com/corpdesk/ansible-testbed.git
 fi
 
 if [ -d "/home/devops/.cb" ] 
 then
-    echo "--------.cb dir exists"
+    echo "--------$hostname: .cb dir exists"
 else
-    echo "--------creating new .cb dir"
+    echo "--------$hostname: creating new .cb dir"
     mkdir .cb
 fi
 
 
 if [ -d "/home/devops/.cb/mysql-shell-scripts/" ] 
 then
-    echo "--------/home/devops/.cb/mysql-shell-scripts/ dir exists"
+    echo "--------$hostname: /home/devops/.cb/mysql-shell-scripts/ dir exists"
 else
-    echo "--------creating new .cb/mysql-shell-scriptsdir"
+    echo "--------$hostname: creating new .cb/mysql-shell-scriptsdir"
     mkdir -p /home/devops/.cb/mysql-shell-scripts/
 fi
 
