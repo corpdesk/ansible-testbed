6520986639ae2741dcec97b8159ebde93d4a86b9
georemo <george.oremo@gmail.com>
2023-08-06 06:41:01 +0300

    -

--- deploy-apps/node-shell-script/setupNodeJs.sh
+++ deploy-apps/node-shell-script/setupNodeJs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
 su $USER
 nvm install --lts
