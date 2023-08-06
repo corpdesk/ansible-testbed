73af96a7aa13f9b72a388dec2d2c061b40d322b9
georemo <george.oremo@gmail.com>
2023-08-07 00:24:15 +0300

    -

--- shared-files/host-update-cluster.sh
+++ shared-files/host-update-cluster.sh
@@ -9,17 +9,17 @@ operator="devops"
 clusterMember="routed-93"
 if [ -d "/home/$adminUser/ansible-testbed" ] 
 then
-    echo "--------cloud-brix files will be updated"
+    echo "--------cloud-brix files for $adminUser will be updated"
     cd /home/$adminUser/ansible-testbed
     git pull
     cd /home/$adminUser/
 else
-    echo "--------updating source files"
+    echo "--------updating source files for $adminUser"
     git clone https://github.com/corpdesk/ansible-testbed.git
 fi
 echo "--------moving cluster-update-dirs.sh to controller machine"
 lxc file push /home/$adminUser/ansible-testbed/shared-files/cluster-update-dirs.sh $clusterMember/home/$operator/.cb/cluster-update-dirs.sh
-echo "--------updating cluser member directories"
+echo "--------updating $clusterMember directories"
 lxc exec $clusterMember -- sh /home/$operator/.cb/cluster-update-dirs.sh
 echo "--------pushing cluster-init-user.sh from $adminUser to $clusterMember"
 lxc file push /home/$adminUser/ansible-testbed/shared-files/cluster-init-user.sh    $clusterMember/home/$operator/.cb/cluster-init-user.sh
