fcc7cbd8a22edea854ac4c41bab0838efa406b92
georemo <george.oremo@gmail.com>
2023-08-06 07:08:23 +0300

    -

--- shared-files/ssh-copy-id.sh
+++ shared-files/ssh-copy-id.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cname=$1
-# cip=$(sudo lxc list -c4 --format csv $cname | cut -d' ' -f1)
+cip=$(sudo lxc list -c4 --format csv $cname | cut -d' ' -f1)
 # cip=$(sudo lxc list "a1" -c 4 | awk '!/IPV4/{ if ( $2 != "" ) print $2}')
 # echo "ip for $cname is $cip"
 # ssh-copy-id -i /home/devops/.ssh/id_rsa.pub devops@$cip
 echo "this host $(hostname)"
 echo "current directory $(pwd)"
-sshpass -f "/home/devops/.cb/p" ssh-copy-id -i /home/devops/.ssh/id_rsa.pub devops@$cname
+sshpass -f "/home/devops/.cb/p" ssh-copy-id -i /home/devops/.ssh/id_rsa.pub devops@$cip
 
 
