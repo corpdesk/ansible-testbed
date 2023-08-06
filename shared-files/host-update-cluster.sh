922ec690dada1d962d542a92e94d1ff60307b0f7
georemo <george.oremo@gmail.com>
2023-08-07 00:18:18 +0300

    -

--- shared-files/host-update-cluster.sh
+++ shared-files/host-update-cluster.sh
@@ -3,6 +3,7 @@
 # use this on physical machine to update files
 bash -c '
 # executed at the physical machine
+echo "--------executing at the physical machine"
 adminUser="emp-09"
 operator="devops"
 clusterMember="routed-93"
@@ -16,11 +17,14 @@ else
     echo "--------updating source files"
     git clone https://github.com/corpdesk/ansible-testbed.git
 fi
-
+echo "--------moving cluster-update-dirs.sh to controller machine"
 lxc file push /home/$adminUser/ansible-testbed/shared-files/cluster-update-dirs.sh $clusterMember/home/$operator/.cb/cluster-update-dirs.sh
-lxc exec $clusterMember -- sh $clusterMember/home/$operator/.cb/cluster-update-dirs.sh
-
+echo "--------updating cluser member directories"
+lxc exec $clusterMember -- sh /home/$operator/.cb/cluster-update-dirs.sh
+echo "--------pushing cluster-init-user.sh from $adminUser to $clusterMember"
 lxc file push /home/$adminUser/ansible-testbed/shared-files/cluster-init-user.sh    $clusterMember/home/$operator/.cb/cluster-init-user.sh
+echo "--------pushing worker-init-user.sh from $adminUser to $clusterMember"
 lxc file push /home/$adminUser/ansible-testbed/shared-files/worker-init-user.sh     $clusterMember/home/$operator/.cb/worker-init-user.sh
+echo "--------pushing shared-files/p from $adminUser to $clusterMember"
 lxc file push /home/$adminUser/ansible-testbed/shared-files/p                       $clusterMember/home/$operator/.cb/p
 '
