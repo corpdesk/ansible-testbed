c0cb3edbe285c5b8ed5cfd52ec01be392fad8d9e
georemo <george.oremo@gmail.com>
2023-08-08 01:07:14 +0300

    -

--- shared-files/ssh-copy-id.sh
+++ shared-files/ssh-copy-id.sh
@@ -10,11 +10,11 @@ echo "--------$(hostname)/ssh-copy-id.sh: ssh-copy-id for $cname"
 cip=$(sudo lxc list -c4 --format csv $cname | cut -d' ' -f1)
 # cip=$(sudo lxc list "a1" -c 4 | awk '!/IPV4/{ if ( $2 != "" ) print $2}')
 echo "--------$(hostname)/ssh-copy-id.sh: $cname ip address is $cip"
-export CIP=$cip
+# export CIP=$cip
 # echo "ip for $cname is $cip"
 # ssh-copy-id -i /home/devops/.ssh/id_rsa.pub devops@$cip
 echo "--------$(hostname)/ssh-copy-id.sh: current directory $(pwd)"
-sudo -H -u devops bash -c '
+# sudo -H -u devops bash -c '
 
 if [ -f "/home/devops/.ssh/id_rsa" ] 
 then
@@ -24,8 +24,8 @@ else
     # ssh-keygen -t rsa -b 2048 -f /home/devops/.ssh/id_rsa -q -N ""
     sh /home/devops/.cb/ssh-key.sh devops
 fi
-echo "cmd: sshpass -f /home/devops/.cb/p ssh-copy-id -i /home/devops/.ssh/id_rsa.pub devops@$CIP"
-sshpass -f "/home/devops/.cb/p" ssh-copy-id -i /home/devops/.ssh/id_rsa.pub devops@$CIP
-'
+echo "cmd: sshpass -f /home/devops/.cb/p ssh-copy-id -i /home/devops/.ssh/id_rsa.pub devops@$cip"
+sshpass -f "/home/devops/.cb/p" ssh-copy-id -i /home/devops/.ssh/id_rsa.pub devops@$cip
+# '
 
 
