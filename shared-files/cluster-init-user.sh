de4f2f21e4eab6a26dd188cfe52c3adc9f827182
georemo <george.oremo@gmail.com>
2023-08-07 12:32:12 +0300

    -

--- shared-files/cluster-init-user.sh
+++ shared-files/cluster-init-user.sh
@@ -3,29 +3,35 @@ echo "."
 echo "."
 echo "."
 echo "--------STARTING cluster-init-user.sh"
-echo "--------executing at $(hostname)"
+echo "--------$(hostname)/cluster-init-user.sh: executing at $(hostname)"
+echo "--------$(hostname)/cluster-init-user.sh: removing user devops"
 sudo deluser devops
 sudo rm -r -f /home/devops
 # add devops user
+echo "--------$(hostname)/cluster-init-user.sh: adding user devops"
 sudo useradd -m -p $(openssl passwd -1 yU0B14NC1PdE) devops
 if [ -d "/home/devops/.cb/mysql-shell-scripts/" ] 
 then
-    echo "--------/home/devops/.cb/mysql-shell-scripts/ dir exists"
+    echo "--------$(hostname)/cluster-init-user.sh: /home/devops/.cb/mysql-shell-scripts/ dir exists"
 else
-    echo "--------creating new .cb/mysql-shell-scriptsdir"
+    echo "--------$(hostname)/cluster-init-user.sh: creating new .cb/mysql-shell-scriptsdir"
     mkdir -p /home/devops/.cb/mysql-shell-scripts/
 fi
+echo "--------$(hostname)/cluster-init-user.sh: setting devops profile"
 sudo chmod -R 755 /home/devops/
 # escalate devops to sudoer
 sudo usermod -aG sudo devops
 sudo cp /etc/sudoers /etc/sudoers.backup
 # suppress password prompt on switch to user
 sudo bash -c 'echo "devops ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers'
+echo "--------$(hostname)/cluster-init-user.sh: setting up ssh access"
 # enable password authentication for ssh connection
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
 sudo sed -i -E 's/#?PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo systemctl restart ssh
+
 for i in {1..3}
 do
+    echo "--------$(hostname)/cluster-init-user.sh: ssh-copy-id to cd-db-0$i"
     sh /home/devops/ansible-testbed/shared-files/ssh-copy-id.sh cd-db-0$i
 done
