a0a3107cf06e342599684cd52ff12240522e75cb
georemo <george.oremo@gmail.com>
2023-08-07 15:56:17 +0300

    -

--- shared-files/cluster-update-worker.sh
+++ shared-files/cluster-update-worker.sh
@@ -17,7 +17,8 @@ if [ -d "/home/$operator/ansible-testbed" ]
 then
     echo "--------$(hostname)/cluster-update-worker.sh: cloud-brix files for $clusterMember will be updated"
     cd /home/$operator/ansible-testbed
-    git fetch --all
+    # git fetch --all
+    git pull --force "@{u}:HEAD
     cd /home/$operator/
 else
     echo "--------$(hostname)/cluster-update-worker.sh: updating source files for $clusterMember"
