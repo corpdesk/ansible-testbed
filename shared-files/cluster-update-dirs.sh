219ce0e7b67729f9dc6fcbaba653bebc4f830fae
georemo <george.oremo@gmail.com>
2023-08-07 13:54:37 +0300

    -

--- shared-files/cluster-update-dirs.sh
+++ shared-files/cluster-update-dirs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # script executed at a cluster member (ansible controller) assuming initial user(devops) has alreaydy been setup
+
 clusterMember="routed-93"
 lxc exec $clusterMember -- sudo -H -u devops bash -c '
 clusterMember="routed-93"
@@ -8,34 +9,34 @@ echo "."
 echo "."
 echo "."
 echo "--------STARTING cluster-update-dirs.sh"
-echo "--------executing at $(hostname)"
-echo "--------cluster-update-dirs.sh: executing at the cluster member $clusterMember"
+echo "--------$(hostname)/cluster-update-dirs.sh: executing at $(hostname)"
+echo "--------$(hostname)/cluster-update-dirs.sh: executing at the cluster member $clusterMember"
 if [ -d "/home/devops/ansible-testbed" ] 
 then
-    echo "--------cluster-update-dirs.sh: cloud-brix files will be updated at $(hostname)"
+    echo "--------$(hostname)/cluster-update-dirs.sh: cloud-brix files will be updated at $(hostname)"
     cd /home/devops/ansible-testbed
     git pull
     cd /home/devops/
 else
-    echo "--------cluster-update-dirs.sh: updating source files at $(hostname)"
+    echo "--------$(hostname)/cluster-update-dirs.sh: updating source files at $(hostname)"
     cd /home/devops/
     git clone https://github.com/corpdesk/ansible-testbed.git
 fi
 
 if [ -d "/home/devops/.cb" ] 
 then
-    echo "--------cluster-update-dirs.sh: $(hostname): .cb dir exists"
+    echo "--------$(hostname)/cluster-update-dirs.sh: $(hostname): .cb dir exists"
 else
-    echo "--------cluster-update-dirs.sh: $(hostname): creating new .cb dir"
+    echo "--------$(hostname)/cluster-update-dirs.sh: $(hostname): creating new .cb dir"
     mkdir .cb
 fi
 
 
 if [ -d "/home/devops/.cb/mysql-shell-scripts/" ] 
 then
-    echo "--------cluster-update-dirs.sh: $(hostname): /home/devops/.cb/mysql-shell-scripts/ dir exists"
+    echo "--------$(hostname)/cluster-update-dirs.sh: $(hostname): /home/devops/.cb/mysql-shell-scripts/ dir exists"
 else
-    echo "--------cluster-update-dirs.sh: $(hostname): creating new .cb/mysql-shell-scriptsdir"
+    echo "--------$(hostname)/cluster-update-dirs.sh: $(hostname): creating new .cb/mysql-shell-scriptsdir"
     mkdir -p /home/devops/.cb/mysql-shell-scripts/
 fi
 
