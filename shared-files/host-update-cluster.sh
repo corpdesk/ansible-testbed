fe78dd3d4c7bcdb6305e236d559aef9ff5847859
georemo <george.oremo@gmail.com>
2023-08-10 17:20:05 +0300

    -

--- shared-files/host-update-cluster.sh
+++ shared-files/host-update-cluster.sh
@@ -5,11 +5,18 @@
 # execute below in at any host (emp-09, emp-10, emp-11) 
 # cd ~/ansible-testbed && git pull && sh ~/ansible-testbed/shared-files/host-update-cluster.sh
 
+# # executing concatenated commands
+# cmd1="echo xx"
+# cmd2="echo yy"
+# cmd="$cmd1; $cmd2"
+# bash -c "$cmd"
+
 l1Hostname="emp-09"
 l1Operator="emp-09"
 
 l1Hostname="routed-93"
 l2Operator="devops"
+cmdHead=""
 
 bash -c '
 # executed at the physical machine
@@ -19,9 +26,9 @@ echo "."
 echo "--------$(hostname)/STARTING host-update-cluster.sh"
 echo "--------$(hostname)/cluster-init-user.sh: whoami: $(whoami)"
 echo "--------$(hostname)/host-update-cluster.sh: executing at the physical machine"
-adminUser=${l1Operator}
-operator=${l2Operator}
-clusterMember=${l1Hostname}
+adminUser="emp-09"
+operator="devops"
+clusterMember="routed-93"
 if [ -d "/home/$adminUser/ansible-testbed" ] 
 then
     echo "--------$(hostname)/host-update-cluster.sh: cloud-brix files for $adminUser will be updated"
---
+++ shared-files/test-shell.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# # executing concatenated commands
+# cmd1="echo xx"
+# cmd2="echo yy"
+# cmd="$cmd1; $cmd2"
+# bash -c "$cmd"
+# -----------------------------------------------------------
+
+# # exported string variable & function output
+# **************************************************
+# export ENV_VAR="env-var"
+# fxPringSomething(){
+#     echo "fxSomething";
+# }
+# cmdHead='echo "."
+# echo "."
+# echo "."
+# echo "--------$(hostname)/STARTING host-update-cluster.sh"
+# echo "--------$(hostname)/cluster-init-user.sh: whoami: $(whoami)"
+# echo "--------$(hostname)/host-update-cluster.sh: executing at the physical machine"'
+# cmd2="echo ${ENV_VAR}"
+# cmd3="echo $(fxPringSomething)"
+# cmd="$cmdHead; $cmd2; $cmd3;"
+# bash -c "$cmd"
+# -----------------------------------------------------------------
+
+adminUser="emp-09"
+operator="devops"
+clusterMember="routed-93"
+export ENV_VAR="env-var"
+
+fxPringSomething(){
+    echo "fxSomething";
+}
+
+gitAnsibleTestbed(){
+    if [ -d "/home/emp-06/test/ansible-testbed" ] 
+    then
+        echo "--------$(hostname)/host-update-cluster.sh: cloud-brix files for $adminUser  will be updated"
+        cd /home/emp-06/test/ansible-testbed
+        git pull
+        cd /home/emp-06/
+    else
+        mkdir -p /home/emp-06/test/
+        cd /home/emp-06/test/
+        echo "--------$(hostname)/host-update-cluster.sh: updating source files for $adminUser"
+        git clone https://github.com/corpdesk/ansible-testbed.git
+    fi
+
+}
+
+cmdHead='echo "."
+echo "."
+echo "."
+echo "--------$(hostname)/STARTING host-update-cluster.sh"
+echo "--------$(hostname)/cluster-init-user.sh: whoami: $(whoami)"
+echo "--------$(hostname)/host-update-cluster.sh: executing at the physical machine"'
+cmd2="echo ${ENV_VAR}"
+cmdGitAnsibleTestbed="echo $(gitAnsibleTestbed)"
+
+cmd="$cmdHead; $cmd2; $cmdGitAnsibleTestbed;"
+bash -c "$cmd"
+
+
