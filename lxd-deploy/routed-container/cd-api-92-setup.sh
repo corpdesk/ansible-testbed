dbea3406a1bd1fad64f2b6cb3315973889fe6565
georemo <george.oremo@gmail.com>
2024-04-02 10:31:43 +0300

    -

--- lxd-deploy/routed-container/cd-api-92-setup.sh
+++ lxd-deploy/routed-container/cd-api-92-setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+lxc_container="c1"
+repoDir="$HOME/temp/"
+app_file="cd-api.zip"
 
 # install npm and typescript
+echo "----------------------------------------------------"
+echo "INSTALL npm AND typescript"
+echo "----------------------------------------------------"
 lxc exec c1 -- sudo -H -u devops bash -c '
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
@@ -9,20 +15,27 @@ nvm install v16.20.1
 npm install -g typescript
 npm install -g ts-node
 npm install -g zx
+npm --version
 '
-lxc_container="c1"
-repoDir="$HOME/temp/"
-app_file="cd-api.zip"
+
 # push cd-api compressed source
+echo "----------------------------------------------------"
+echo "DEPLOY cd-api"
+echo "----------------------------------------------------"
 echo -e "-- Push $app_file file to $lxc_container/home/devops"
-lxc file push "$repoDir$app_file" $lxc_container/home/devops
+lxc file push $HOME/temp/cd-api.zip c1/home/devops/
 # change ownership
-lxc exec c1 -- sudo chown devops /home/devops/$app_file
-# change permissions
+echo -e "-- set ownership"
+lxc exec $lxc_container -- sudo chown devops /home/devops/$app_file
 
 # unzip cd-api source
-lxc exec cd-shell-24 -- sudo -H -u devops bash -c '
-unzip /home/devops/cd-api.zip -d /home/devops/cd-api
+echo -e "uncompress cd-api"
+lxc exec $lxc_container -- sudo -H -u devops bash -c '
+unzip /home/devops/cd-api.zip -d /home/devops
 '
+
+# configure hosts
+
+sh cd-api-start.sh
 
 
