db2bbc2ab6b753c552390ac11275bceea3d75021
georemo <george.oremo@gmail.com>
2024-04-05 09:35:09 +0300

    -

--- shared-files/scp.sh
+++ shared-files/scp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # preceed this procedure with the following to avoid password prompt
-# ssh-copy-id emp-07@remote_host
+# ssh-copy-id emp-07@emp-07
 
 # Define variables
-REMOTE_HOST="username@remote_host"
-REMOTE_PATH="/remote/path/to/destination"
-LOCAL_FILE="/local/path/to/file"
+REMOTE_HOST="emp-07@emp-07"
+REMOTE_PATH="/home/emp-07/temp"
+LOCAL_FILE="/home/emp-06/snap/mysql-workbench-community/13/dumps/Dump20240404.sql"
 
 # Use SCP to transfer the file
 scp "$LOCAL_FILE" "$REMOTE_HOST":"$REMOTE_PATH"
