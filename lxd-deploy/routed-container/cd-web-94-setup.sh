77b7cd71d42c9e7c09806a4169eac5778b2db7ae
georemo <george.oremo@gmail.com>
2024-04-08 21:33:40 +0300

    -

--- lxd-deploy/routed-container/cd-web-94-setup.sh
+++ lxd-deploy/routed-container/cd-web-94-setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 lxc_container="cd-web-94"
-apache_installer="/home/emp-07/ansible-testbed/vrrp/webserver.sh"
+setup_dir="/home/emp-07/ansible-testbed/vrrp/"
+setup_file="webserver.sh"
 
 echo -e "-- Push $setup_file file to $lxc_container/temp/"
-lxc file push $setup_file $lxc_container/temp/
+lxc file push $setup_dir$setup_file $lxc_container/temp/
 lxc exec $lxc_container -- sh /temp/$setup_file
