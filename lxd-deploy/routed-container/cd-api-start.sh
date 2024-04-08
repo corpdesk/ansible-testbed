3fb1032a0c893d5d7ce64d92a7c8a8fd77e10a50
“georemo” <george.oremo@gmail.com>
2024-04-08 21:24:02 +0300

    -

--- lxd-deploy/routed-container/cd-api-start.sh
+++ lxd-deploy/routed-container/cd-api-start.sh
@@ -4,6 +4,10 @@ lxc stop cd-db-91
 sleep 2
 lxc start cd-db-91
 sleep 5
+lxc stop cd-api-92
+sleep 2
+lxc start cd-api-92
+sleep 5
 
 # cd-api start
 lxc exec cd-api-92 -- sudo -H -u devops bash -c '
--- lxd-deploy/routed-container/cd-sio-start.sh
+++ lxd-deploy/routed-container/cd-sio-start.sh
@@ -4,6 +4,11 @@ lxc stop cd-db-91
 sleep 2
 lxc start cd-db-91
 sleep 5
+lxc stop cd-sio-93
+sleep 2
+lxc start cd-sio-93
+sleep 5
+
 
 # cd-sio start
 lxc exec cd-sio-93 -- sudo -H -u devops bash -c '
